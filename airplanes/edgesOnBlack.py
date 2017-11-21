8d8be4638e01c951b1dfc1630aed5e8a873bc457
Piotr Ptak <robakzdobywca@gmail.com>
2017-11-21 21:48:34 +0100

    better contour finding, with smoothing filter, erosion and dilatation

--- airplanes/edgesOnBlack.py
+++ airplanes/edgesOnBlack.py
@@ -18,9 +18,10 @@ projectDirectory = os.getcwd()
 #cool selection of colors ( ͡° ͜ʖ ͡°)
 myColors = ["#e25724", "#ffd149", "#ffff49", "#a4ff49", "#59bf3d", "#33cea2", "#40cce5", "#37a0fc", "#4655d6", "#825ff4", "#e539bd", "#fc1964"]
 
-qthPercentile = 4
-contourLevelValue = 0.13
-contourWidth = 1.8
+#parameters used for filtering and contour finding
+qthPercentile = 12
+contourLevelValue = 0.3
+contourWidth = 1.4
 airplanes = []
 contours = []
 
@@ -38,18 +39,35 @@ def collectImages(baseName, lastNumber):
 def drawPictureWithContour(image, x):
     fig = plt.figure()
     #rescale exposure
+    min = np.percentile(image, 5)
     perc = np.percentile(image, qthPercentile)
-    resc = exposure.rescale_intensity(image,in_range=(0, perc))
+    resc = exposure.rescale_intensity(image,in_range=(min, perc))
 
+    #kernel is used for dilatation and erosion
+    kernel = np.ones((5,5),np.uint8)
+
+    #we will be changing the outcome depending on the image value, hence the hsv format
     img = color.rgb2hsv(resc)
     height, width, channels = img.shape
     #print("width: " + str(width))
     #print("height: " + str(height))
     #print("channels: " + str(channels))
+
+    #create 'inverse' array
     inv = np.zeros([height, width])
     for i in range(height):
         for j in range(width):
+            #sky is a lot brighter than planes. inverse array will contain the data used for edge finding
             inv[i][j] = 1 - img[i][j][2]
+
+    #time to smooth the results
+    inv = gaussian(inv, sigma=0.8)
+
+    #erosion and dilatation to patch up objects on the sky
+    inv = cv2.erode(inv,kernel,iterations = 1)
+    inv = cv2.dilate(inv,kernel,iterations = 2)
+
+    #actual contour finding
     contours = measure.find_contours(inv, contourLevelValue)
     for n, contours in enumerate(contours):
         plt.plot(contours[:,1],contours[:,0],linewidth=contourWidth, color=rcg(myColors))
@@ -58,8 +76,8 @@ def drawPictureWithContour(image, x):
     opencv represents rgb images as nd-array, but in reverse order (they are bgr instead of rgb)
     we have to convert them back to rgb using COLOR_BGR2RGB function
     '''
-    #plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.imshow(inv)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    #plt.imshow(image)
     #plt.show()
     fig.savefig(str(x) + ".pdf")
 
